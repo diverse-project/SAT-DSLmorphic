0194a53c316ed21a2e395ce1da64533fcdad0719
Gwendal <gwendal.jouneaux@etudiant.univ-rennes1.fr>
2019-09-30 15:56:36 +0200

    simplify complex operators to simple operators

--- org.xtext.example.sat/src/org/xtext/example/mydsl/generator/Simplifier.xtend
+++ org.xtext.example.sat/src/org/xtext/example/mydsl/generator/Simplifier.xtend
@@ -3,13 +3,15 @@ package org.xtext.example.mydsl.generator
 import org.eclipse.emf.ecore.EObject
 import org.xtext.example.mydsl.sat.And
 import org.xtext.example.mydsl.sat.BiImpl
+import org.xtext.example.mydsl.sat.Expression
 import org.xtext.example.mydsl.sat.Impl
 import org.xtext.example.mydsl.sat.Nand
 import org.xtext.example.mydsl.sat.Not
 import org.xtext.example.mydsl.sat.Or
+import org.xtext.example.mydsl.sat.SatFactory
 
 class Simplifier {
-	static def EObject simplify(EObject e){
+	static def Expression simplify(EObject e){
 		switch e{
 			case e instanceof BiImpl : return (e as BiImpl).simplify
 			case e instanceof Impl   : return (e as Impl)  .simplify
@@ -17,31 +19,73 @@ class Simplifier {
 			case e instanceof And    : return (e as And)   .simplify
 			case e instanceof Or     : return (e as Or)    .simplify
 			case e instanceof Not    : return (e as Not)   .simplify
-			default                  : return  e
+			default                  : return  e as Expression
 		}
 	}
 	
-	static def EObject simplify(BiImpl e){
+	static def Expression simplify(BiImpl e){
+		val lhsReplacement = simplify(e.left)
+		val rhsReplacement = simplify(e.right)
+		
+		val andTrue   = SatFactory.eINSTANCE.createAnd
+		val andFalse  = SatFactory.eINSTANCE.createAnd
+		val notLeft   = SatFactory.eINSTANCE.createNot
+		val notRight  = SatFactory.eINSTANCE.createNot
+		val or        = SatFactory.eINSTANCE.createOr
+		
+		andTrue.left = lhsReplacement
+		andTrue.right = rhsReplacement
+		
+		notLeft.expression  = lhsReplacement
+		notRight.expression = rhsReplacement
+		
+		andFalse.left = notLeft
+		andFalse.right = notRight
+		
+		or.left = andTrue
+		or.right = andFalse
+		
+		return or
+	}
+	
+	static def Expression simplify(Impl e){
+		val lhsReplacement = simplify(e.left)
+		val rhsReplacement = simplify(e.right)
+		
+		val or  = SatFactory.eINSTANCE.createOr
+		val not = SatFactory.eINSTANCE.createNot
+		
+		not.expression = lhsReplacement
+		
+		or.left = not
+		or.right = rhsReplacement
+		
+		return or
+	}
+	
+	static def Expression simplify(Nand e){
+		val lhsReplacement = simplify(e.left)
+		val rhsReplacement = simplify(e.right)
+		
+		val and = SatFactory.eINSTANCE.createAnd
+		val not = SatFactory.eINSTANCE.createNot
+		
+		and.left = lhsReplacement
+		and.right = rhsReplacement
+		not.expression = and
+		
+		return not
+	}
+	
+	static def Expression simplify(And e){
 		e
 	}
 	
-	static def EObject simplify(Impl e){
+	static def Expression simplify(Or e){
 		e
 	}
 	
-	static def EObject simplify(Nand e){
-		e
-	}
-	
-	static def EObject simplify(And e){
-		e
-	}
-	
-	static def EObject simplify(Or e){
-		e
-	}
-	
-	static def EObject simplify(Not e){
+	static def Expression simplify(Not e){
 		e
 	}
 }
--- org.xtext.example.sat/xtend-gen/org/xtext/example/mydsl/generator/Simplifier.java
+++ org.xtext.example.sat/xtend-gen/org/xtext/example/mydsl/generator/Simplifier.java
@@ -3,14 +3,16 @@ package org.xtext.example.mydsl.generator;
 import org.eclipse.emf.ecore.EObject;
 import org.xtext.example.mydsl.sat.And;
 import org.xtext.example.mydsl.sat.BiImpl;
+import org.xtext.example.mydsl.sat.Expression;
 import org.xtext.example.mydsl.sat.Impl;
 import org.xtext.example.mydsl.sat.Nand;
 import org.xtext.example.mydsl.sat.Not;
 import org.xtext.example.mydsl.sat.Or;
+import org.xtext.example.mydsl.sat.SatFactory;
 
 @SuppressWarnings("all")
 public class Simplifier {
-  public static EObject simplify(final EObject e) {
+  public static Expression simplify(final EObject e) {
     boolean _matched = false;
     if ((e instanceof BiImpl)) {
       _matched=true;
@@ -46,30 +48,59 @@ public class Simplifier {
         return Simplifier.simplify(((Not) e));
       }
     }
+    return ((Expression) e);
+  }
+  
+  public static Expression simplify(final BiImpl e) {
+    final Expression lhsReplacement = Simplifier.simplify(e.getLeft());
+    final Expression rhsReplacement = Simplifier.simplify(e.getRight());
+    final And andTrue = SatFactory.eINSTANCE.createAnd();
+    final And andFalse = SatFactory.eINSTANCE.createAnd();
+    final Not notLeft = SatFactory.eINSTANCE.createNot();
+    final Not notRight = SatFactory.eINSTANCE.createNot();
+    final Or or = SatFactory.eINSTANCE.createOr();
+    andTrue.setLeft(lhsReplacement);
+    andTrue.setRight(rhsReplacement);
+    notLeft.setExpression(lhsReplacement);
+    notRight.setExpression(rhsReplacement);
+    andFalse.setLeft(notLeft);
+    andFalse.setRight(notRight);
+    or.setLeft(andTrue);
+    or.setRight(andFalse);
+    return or;
+  }
+  
+  public static Expression simplify(final Impl e) {
+    final Expression lhsReplacement = Simplifier.simplify(e.getLeft());
+    final Expression rhsReplacement = Simplifier.simplify(e.getRight());
+    final Or or = SatFactory.eINSTANCE.createOr();
+    final Not not = SatFactory.eINSTANCE.createNot();
+    not.setExpression(lhsReplacement);
+    or.setLeft(not);
+    or.setRight(rhsReplacement);
+    return or;
+  }
+  
+  public static Expression simplify(final Nand e) {
+    final Expression lhsReplacement = Simplifier.simplify(e.getLeft());
+    final Expression rhsReplacement = Simplifier.simplify(e.getRight());
+    final And and = SatFactory.eINSTANCE.createAnd();
+    final Not not = SatFactory.eINSTANCE.createNot();
+    and.setLeft(lhsReplacement);
+    and.setRight(rhsReplacement);
+    not.setExpression(and);
+    return not;
+  }
+  
+  public static Expression simplify(final And e) {
     return e;
   }
   
-  public static EObject simplify(final BiImpl e) {
+  public static Expression simplify(final Or e) {
     return e;
   }
   
-  public static EObject simplify(final Impl e) {
-    return e;
-  }
-  
-  public static EObject simplify(final Nand e) {
-    return e;
-  }
-  
-  public static EObject simplify(final And e) {
-    return e;
-  }
-  
-  public static EObject simplify(final Or e) {
-    return e;
-  }
-  
-  public static EObject simplify(final Not e) {
+  public static Expression simplify(final Not e) {
     return e;
   }
 }
