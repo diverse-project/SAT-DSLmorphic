600c06c1b5f854f146b17f97f58d329c0c9189d8
ngahy-be <georgesaaron.randrianaina.etu@univ-lille.fr>
2019-10-20 22:11:44 +0200

    bug(maven): does not download dependency

---
+++ maven/src/main/java/org/xtext/example/sat/maven/App.java
@@ -0,0 +1,36 @@
+package org.xtext.example.sat.maven;
+
+/**
+ * Code that tests satisfiability of a dimacs file with sat4j
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	ISolver solver = SolverFactory.newDefault();
+        solver.setTimeout(3600); // 1 hour timeout
+        Reader reader = new DimacsReader(solver);
+        PrintWriter out = new PrintWriter(System.out,true);
+        // CNF filename is given on the command line 
+        try {
+            IProblem problem = reader.parseInstance(args[0]);
+            if (problem.isSatisfiable()) {
+                System.out.println("Satisfiable !");
+                reader.decode(problem.model(),out);
+            } else {
+                System.out.println("Unsatisfiable !");
+            }
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+        } catch (ParseFormatException e) {
+            // TODO Auto-generated catch block
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+        } catch (ContradictionException e) {
+            System.out.println("Unsatisfiable (trivial)!");
+        } catch (TimeoutException e) {
+            System.out.println("Timeout, sorry!");      
+        }
+    }
+}
