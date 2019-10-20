840f73c64619fd3a95a565da264e10ea45760e23
ngahy-be <georgesaaron.randrianaina.etu@univ-lille.fr>
2019-10-20 22:34:38 +0200

    add dependency

--- maven/src/main/java/org/xtext/example/sat/maven/App.java
+++ maven/src/main/java/org/xtext/example/sat/maven/App.java
@@ -1,12 +1,25 @@
 package org.xtext.example.sat.maven;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.sat4j.minisat.SolverFactory;
+import org.sat4j.reader.DimacsReader;
+import org.sat4j.reader.ParseFormatException;
+import org.sat4j.reader.Reader;
+import org.sat4j.specs.ContradictionException;
+import org.sat4j.specs.IProblem;
+import org.sat4j.specs.ISolver;
+import org.sat4j.specs.TimeoutException;
+
 /**
  * Code that tests satisfiability of a dimacs file with sat4j
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws TimeoutException
     {
     	ISolver solver = SolverFactory.newDefault();
         solver.setTimeout(3600); // 1 hour timeout
@@ -22,15 +35,15 @@ public class App
                 System.out.println("Unsatisfiable !");
             }
         } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
+            System.out.println(e.getMessage());
         } catch (ParseFormatException e) {
-            // TODO Auto-generated catch block
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
+            System.out.println(e.getMessage());
+        } catch (TimeoutException e) {
+            System.out.println(e.getMessage());
         } catch (ContradictionException e) {
             System.out.println("Unsatisfiable (trivial)!");
-        } catch (TimeoutException e) {
-            System.out.println("Timeout, sorry!");      
-        }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+		}
     }
 }
