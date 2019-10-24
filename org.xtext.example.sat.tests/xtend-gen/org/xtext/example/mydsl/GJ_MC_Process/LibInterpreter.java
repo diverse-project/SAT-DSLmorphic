dffd8fbfe555a82b530f59e95168c0956c808bf1
Mathieu Acher <mathieu.acher@irisa.fr>
2019-10-24 15:08:58 +0200

    binaries

--- org.xtext.example.sat.tests/xtend-gen/org/xtext/example/mydsl/GJ_MC_Process/LibInterpreter.java
+++ org.xtext.example.sat.tests/xtend-gen/org/xtext/example/mydsl/GJ_MC_Process/LibInterpreter.java
@@ -1,56 +1,23 @@
 package org.xtext.example.mydsl.GJ_MC_Process;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintWriter;
-import org.eclipse.xtext.xbase.lib.Exceptions;
-import org.sat4j.minisat.SolverFactory;
-import org.sat4j.reader.DimacsReader;
-import org.sat4j.reader.ParseFormatException;
-import org.sat4j.specs.ContradictionException;
-import org.sat4j.specs.IProblem;
-import org.sat4j.specs.ISolver;
-import org.sat4j.specs.TimeoutException;
-
 @SuppressWarnings("all")
 public class LibInterpreter {
   public static boolean interpret(final String filename) {
-    ISolver solver = SolverFactory.newDefault();
-    solver.setTimeout(3600);
-    DimacsReader reader = new DimacsReader(solver);
-    PrintWriter out = new PrintWriter(System.out, true);
-    try {
-      IProblem problem = reader.parseInstance(filename);
-      boolean _isSatisfiable = problem.isSatisfiable();
-      if (_isSatisfiable) {
-        System.out.println("Satisfiable !");
-        reader.decode(problem.model(), out);
-      } else {
-        System.out.println("Unsatisfiable !");
-      }
-    } catch (final Throwable _t) {
-      if (_t instanceof FileNotFoundException) {
-        final FileNotFoundException e = (FileNotFoundException)_t;
-        e.printStackTrace();
-        return false;
-      } else if (_t instanceof ParseFormatException) {
-        final ParseFormatException e_1 = (ParseFormatException)_t;
-        e_1.printStackTrace();
-        return false;
-      } else if (_t instanceof IOException) {
-        final IOException e_2 = (IOException)_t;
-        e_2.printStackTrace();
-        return false;
-      } else if (_t instanceof ContradictionException) {
-        System.out.println("Unsatisfiable !");
-        return false;
-      } else if (_t instanceof TimeoutException) {
-        System.out.println("Timeout, sorry!");
-        return false;
-      } else {
-        throw Exceptions.sneakyThrow(_t);
-      }
-    }
-    return true;
+    throw new Error("Unresolved compilation problems:"
+      + "\nParseFormatException cannot be resolved to a type."
+      + "\nContradictionException cannot be resolved to a type."
+      + "\nTimeoutException cannot be resolved to a type."
+      + "\nThe method or field SolverFactory is undefined"
+      + "\nDimacsReader cannot be resolved."
+      + "\nUnreachable code: The catch block can never match. It is already handled by a previous condition."
+      + "\nUnreachable code: The catch block can never match. It is already handled by a previous condition."
+      + "\nUnreachable code: The catch block can never match. It is already handled by a previous condition."
+      + "\nnewDefault cannot be resolved"
+      + "\nsetTimeout cannot be resolved"
+      + "\nparseInstance cannot be resolved"
+      + "\nisSatisfiable cannot be resolved"
+      + "\ndecode cannot be resolved"
+      + "\nmodel cannot be resolved"
+      + "\nprintStackTrace cannot be resolved");
   }
 }
