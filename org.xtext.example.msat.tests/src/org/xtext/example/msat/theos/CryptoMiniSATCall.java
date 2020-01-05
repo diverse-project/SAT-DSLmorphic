2f3537a2ea98cbacafa836b202cba3586fc52134
theo <tgouzien@protonmail.com>
2020-01-05 01:44:21 +0100

    Good Timeout

--- org.xtext.example.msat.tests/src/org/xtext/example/msat/theos/CryptoMiniSATCall.java
+++ org.xtext.example.msat.tests/src/org/xtext/example/msat/theos/CryptoMiniSATCall.java
@@ -16,7 +16,7 @@ public class CryptoMiniSATCall
 	
 	public static List<Object> DoIt(String file_dimacs_formula, String version) 
 	{	
-		int TIMEOUT = 600000;
+		String TIMEOUT = "600";
 
 		check_version(version);
 		String calling_name = "cryptominisat-" + version;
@@ -27,47 +27,22 @@ public class CryptoMiniSATCall
 		try 
 		{
 			List<String> full_command = new ArrayList<String>();
+			full_command.add("timeout");
+			full_command.add(TIMEOUT);
 			full_command.add("./" + calling_name);
 			full_command.add(file_dimacs_formula);
-			
-			long start = System.currentTimeMillis();
 
 			ProcessBuilder pb = new ProcessBuilder(full_command);
 			Process p = pb.start();
 			BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
-			
-			Timer t = new Timer();
-		    t.schedule(new TimerTask() {
-
-		        @Override
-		        public void run() {
-		        	try {
-						Thread.sleep(100);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-		            p.destroy();
-		        }
-		    }, TIMEOUT);
-		    
-		    long finish = System.currentTimeMillis();
-			long timeElapsed = finish - start;
-			
-			if (timeElapsed > TIMEOUT + 50)
-			{
-				return Arrays.asList(false, -1f);
-			}
 
 			boolean result = false;
-			float real_time = 0f;
+			float real_time = -1f;
 			String[] cut_str;
 			
 			String s = "";
 			while((s = in.readLine()) != null)
 			{
-				// DEBUG
-				// System.out.println(s);
 				if (s.contains("CPU time") || s.contains("Total time"))
 				{
 					cut_str = s.substring(0, s.length() - 1).split(" ");
@@ -81,8 +56,6 @@ public class CryptoMiniSATCall
 				{
 					result = true;
 				}
-				
-			    //System.out.println(s);
 			}
 			
 			int status = p.waitFor();
